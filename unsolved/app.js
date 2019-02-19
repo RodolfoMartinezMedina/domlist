593b95c4ea1d3fce9a09b0d75d4711712bcee23e
Rodolfo Martinez <rmm_rody@hotmail.com>
2019-02-18 23:34:22 -0500

    First commit

---
+++ unsolved/app.js
@@ -0,0 +1,13 @@
+const hideFunction = function(){
+    $("#input").hide();
+};
+
+hideFunction();
+
+const showFunction = function(){
+    $("#input").show();
+};
+
+let command = ""
+let command = "view"
+
